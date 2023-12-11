5fa7cf989dc2e3f4ed92daad84da434c2c90bd5d
nerdyscout <nerdyscout@posteo.de>
2023-12-11 21:07:38 +0100

    FreeRTOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
 #include <Arduino.h>
 #include <tusb.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include <queue.h>
+#include <semphr.h>
+
+TaskHandle_t taskBlinkHandle;
+
+void TaskBlink(void *pvParameters)
+{
+  (void)pvParameters;
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  for (;;)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    vTaskDelay(250 / portTICK_PERIOD_MS);
+    digitalWrite(LED_BUILTIN, LOW);
+    vTaskDelay(250 / portTICK_PERIOD_MS);
+  }
+}
 
 void setup(void)
 {
-  tusb_init(); // initialize tinyusb stack
+  xTaskCreate(TaskBlink, // Task function
+              "Blink",   // Task name
+              128,       // Stack size
+              NULL,
+              0,                 // Priority
+              &taskBlinkHandle); // Task handler
 }
 
 void loop(void)
